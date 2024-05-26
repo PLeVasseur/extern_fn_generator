88846db0f1e72c98593a23cf55b0acffb038701c
Peter LeVasseur <peter.levasseur@gm.com>
2024-05-26 10:17:40 -0400

    nice, works without having to specify match arms now

--- src/lib.rs
+++ src/lib.rs
@@ -2,13 +2,14 @@ extern crate proc_macro;
 
 use proc_macro::TokenStream;
 use quote::{quote, format_ident};
-use syn::LitInt;
+use syn::{LitInt, parse_macro_input};
 
 #[proc_macro]
 pub fn generate_extern_fns(input: TokenStream) -> TokenStream {
-    let num_fns = syn::parse_macro_input!(input as LitInt).base10_parse::<usize>().unwrap();
+    let num_fns = parse_macro_input!(input as LitInt).base10_parse::<usize>().unwrap();
 
     let mut generated_fns = quote! {};
+    let mut match_arms = quote! {};
 
     for i in 0..num_fns {
         let extern_fn_name = format_ident!("extern_on_msg_wrapper_{}", i);
@@ -35,7 +36,23 @@ pub fn generate_extern_fns(input: TokenStream) -> TokenStream {
         };
 
         generated_fns.extend(fn_code);
+
+        let match_arm = quote! {
+            #i => #extern_fn_name,
+        };
+        match_arms.extend(match_arm);
     }
 
-    generated_fns.into()
+    let expanded = quote! {
+        #generated_fns
+
+        fn get_extern_fn(listener_id: usize) -> extern "C" fn(u32) {
+            match listener_id {
+                #match_arms
+                _ => panic!("Listener ID out of range"),
+            }
+        }
+    };
+
+    expanded.into()
 }
